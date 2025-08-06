cbbaecb040080857d1c598d749e58527e48f9f9b
rofidarashed <rofidaelmaghraby@gmail.com>
2025-08-07 02:08:14 +0300

    add app colors , animations

---
+++ lib/core/app_animations.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppAnimations {
+  static late Animation<double> fadeAnimation;
+  static late Animation<Offset> slideAnimation;
+  static late Animation<double> scaleAnimation;
+  static late Animation<double> glowAnimation;
+  static late Animation<double> incrementAnimation;
+  static late Animation<double> decrementAnimation;
+}
---
+++ lib/core/app_colors.dart
@@ -0,0 +1,23 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF6C5CE7);
+  static const Color secondaryColor = Color(0xFF74B9FF);
+  static const Color cardColor = Color(0xFF252F3F);
+  static const Color surfaceColor = Color(0xFF1A2332);
+  static const Color textPrimary = Color(0xFFFFFFFF);
+  static const Color textSecondary = Color(0xFFB2BAC2);
+  static const Color accentColor = Color(0xFF00CEC9);
+  static const Color backgroundColor = Color(0xFF0D1421);
+
+  static const Color femaleColor = Color(0xFFFF6B9D);
+  static const Color maleColor = Color(0xFF6586e6);
+
+  static const Color malePrimary = Color(0xFF6C5CE7);
+  static const Color maleSecondary = Color(0xFF74B9FF);
+
+  static const Color femalePrimary = Color(0xFFE84393);
+  static const Color femaleSecondary = Color(0xFFF8A5C2);
+
+  
+}
