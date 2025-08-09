bfd92eea6b27b19f890aa14b66dae1a4d8a0845c
rofidarashed <rofidaelmaghraby@gmail.com>
2025-08-09 18:44:07 +0300

    add screenutil

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,32 @@
-import 'package:bmi/my_app.dart';
+import 'package:bmi/feature/bmi/screens/bmi_input_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() => runApp(const MyApp());
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(360, 788),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_, child) {
+        return MaterialApp(
+          title: 'BMI Calculator',
+          theme: ThemeData.dark().copyWith(
+            scaffoldBackgroundColor: const Color(0xFF0A0E21),
+            appBarTheme: const AppBarTheme(
+              backgroundColor: Color(0xFF1D1E33),
+              centerTitle: true,
+            ),
+          ),
+          home: const BMIInputScreen(),
+          debugShowCheckedModeBanner: false,
+        );
+      },
+    );
+  }
+}
