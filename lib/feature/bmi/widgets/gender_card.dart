f14705863a9e23dd67f8f1a8f9ad842db69ffd70
rofidarashed <rofidaelmaghraby@gmail.com>
2025-08-10 01:52:23 +0300

    update gender card

--- lib/feature/bmi/widgets/gender_card.dart
+++ lib/feature/bmi/widgets/gender_card.dart
@@ -1,6 +1,8 @@
-import 'package:bmi/core/app_animations.dart';
+// ignore_for_file: unused_field
+
 import 'package:bmi/core/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class GenderCard extends StatefulWidget {
   final bool isSelected;
@@ -25,6 +27,8 @@ class GenderCard extends StatefulWidget {
 class _GenderCardState extends State<GenderCard>
     with SingleTickerProviderStateMixin {
   late AnimationController _animationController;
+  late Animation<double> _scaleAnimation;
+  late Animation<double> _glowAnimation;
 
   @override
   void initState() {
@@ -33,10 +37,10 @@ class _GenderCardState extends State<GenderCard>
       duration: const Duration(milliseconds: 200),
       vsync: this,
     );
-    AppAnimations.scaleAnimation = Tween<double>(begin: 1.0, end: 0.95).animate(
+    _scaleAnimation = Tween<double>(begin: 1.0, end: 0.95).animate(
       CurvedAnimation(parent: _animationController, curve: Curves.easeInOut),
     );
-    AppAnimations.glowAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+    _glowAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
       CurvedAnimation(parent: _animationController, curve: Curves.easeInOut),
     );
   }
@@ -57,9 +61,9 @@ class _GenderCardState extends State<GenderCard>
       animation: _animationController,
       builder: (context, child) {
         return Transform.scale(
-          scale: AppAnimations.scaleAnimation.value,
+          scale: _scaleAnimation.value,
           child: Container(
-            margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+            margin: EdgeInsets.symmetric(horizontal: 8.w, vertical: 8.h),
             child: Material(
               color: Colors.transparent,
               child: InkWell(
@@ -69,7 +73,7 @@ class _GenderCardState extends State<GenderCard>
                   });
                   widget.onTap();
                 },
-                borderRadius: BorderRadius.circular(20),
+                borderRadius: BorderRadius.circular(20.r),
                 child: AnimatedContainer(
                   duration: const Duration(milliseconds: 100),
                   curve: Curves.easeInOut,
@@ -77,25 +81,25 @@ class _GenderCardState extends State<GenderCard>
                     color: widget.isSelected
                         ? selectedColor.withOpacity(0.1)
                         : unselectedColor,
-                    borderRadius: BorderRadius.circular(20),
+                    borderRadius: BorderRadius.circular(20.r),
                     border: Border.all(
                       color: widget.isSelected
                           ? selectedColor
                           : Colors.transparent,
-                      width: 2,
+                      width: 2.w,
                     ),
                     boxShadow: widget.isSelected
                         ? [
                             BoxShadow(
                               color: selectedColor.withOpacity(0.3),
-                              blurRadius: 20,
+                              blurRadius: 20.r,
                               offset: const Offset(0, 8),
                             ),
                           ]
                         : [
                             BoxShadow(
                               color: Colors.black.withOpacity(0.1),
-                              blurRadius: 8,
+                              blurRadius: 8.r,
                               offset: const Offset(0, 4),
                             ),
                           ],
@@ -105,7 +109,7 @@ class _GenderCardState extends State<GenderCard>
                     children: [
                       AnimatedContainer(
                         duration: const Duration(milliseconds: 120),
-                        padding: const EdgeInsets.all(16),
+                        padding: EdgeInsets.all(16.r),
                         decoration: BoxDecoration(
                           color: widget.isSelected
                               ? selectedColor.withOpacity(0.2)
@@ -114,17 +118,17 @@ class _GenderCardState extends State<GenderCard>
                         ),
                         child: Icon(
                           widget.icon,
-                          size: 48,
+                          size: 48.r,
                           color: widget.isSelected
                               ? selectedColor
                               : AppColors.textSecondary,
                         ),
                       ),
-                      const SizedBox(height: 16),
+                      SizedBox(height: 16.h),
                       Text(
                         widget.label,
                         style: TextStyle(
-                          fontSize: 16,
+                          fontSize: 16.sp,
                           fontWeight: FontWeight.w600,
                           color: widget.isSelected
                               ? selectedColor
@@ -133,13 +137,13 @@ class _GenderCardState extends State<GenderCard>
                         ),
                       ),
                       if (widget.isSelected) ...[
-                        const SizedBox(height: 8),
+                        SizedBox(height: 8.h),
                         Container(
-                          width: 40,
-                          height: 3,
+                          width: 40.w,
+                          height: 3.h,
                           decoration: BoxDecoration(
                             color: selectedColor,
-                            borderRadius: BorderRadius.circular(2),
+                            borderRadius: BorderRadius.circular(2.r),
                           ),
                         ),
                       ],
